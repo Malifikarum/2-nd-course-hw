b377b6f0aed2e6feb223e5812fdad839175bd46b
Volkov Pavel <biowulf1504@yandex.ru>
2023-05-26 21:09:02 +0300

    Макет готов

---
+++ hw3.js
@@ -0,0 +1,69 @@
+//Задание 1
+let password = 'пароль';
+let userPassword = prompt('Введите пароль');
+let result = password === userPassword ? 'Пароль введен верно' : 'Пароль введен неправильно';
+console.log (result);
+
+//Задание 2
+let c = 2;
+if (c > 0 && c < 10) {
+    console.log('Верно');
+}
+else {
+    console.log('Не верно');
+}
+
+//Задание 3
+let d = 98;
+let e = 113;
+if (d > 100 || e > 100){
+    console.log('Верно');
+}
+else {
+    console.log('Не верно');
+}
+
+//Задание 4
+let a = '2';
+let b = '3';
+a = Number(a);
+b = Number(b);
+console.log(a + b);
+
+//Задание 5
+let monthNumber = 13;
+switch (monthNumber) {
+    case 12:
+    case 1:
+    case 2:
+        console.log('Зима');
+        break;
+    case 3:
+    case 4:
+    case 5:
+        console.log('Весна');
+        break;
+    case 6:
+    case 7:
+    case 8:
+        console.log('Лето');
+        break;    
+    case 9:
+    case 10:
+    case 11:
+    console.log('Осень');
+    break;
+    default: console.log('Такого месяца не существует');
+        break;
+}
+
+//Задание 6
+
+
+//Задание 7
+
+
+//Задание 8
+
+
+//Задание 9
