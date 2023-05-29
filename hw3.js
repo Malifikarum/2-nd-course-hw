52f88cfd6e99fdf3e9c6511bc95796f163d99b8a
Volkov Pavel <biowulf1504@yandex.ru>
2023-05-29 19:48:27 +0300

    create hw4.js

--- hw3.js
+++ hw3.js
@@ -1,8 +1,8 @@
 //Задание 1
 let password = 'пароль';
 let userPassword = prompt('Введите пароль');
-let result = password === userPassword ? 'Пароль введен верно' : 'Пароль введен неправильно';
-console.log (result);
+let resultPassword = password === userPassword ? 'Пароль введен верно' : 'Пароль введен неправильно';
+console.log (resultPassword);
 
 //Задание 2
 let c = 2;
